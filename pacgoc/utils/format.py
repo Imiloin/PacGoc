9b642a311c4909c4cb5ec6c1681b7598fd1b7c21
Imiloin <finvoler6@gmail.com>
2024-07-14 20:34:35 +0800

    do not overwrite separation res when separation is off

--- pacgoc/utils/format.py
+++ pacgoc/utils/format.py
@@ -5,18 +5,19 @@ def pcm16to32(audio):
     """
     Convert PCM 16-bit audio to 32-bit float audio.
     """
-    assert (audio.dtype == np.int16)
+    assert audio.dtype == np.int16
     audio = audio.astype("float32")
     bits = np.iinfo(np.int16).bits
-    audio = audio / (2**(bits - 1))
+    audio = audio / (2 ** (bits - 1))
     return audio
+
 
 def pcm32to16(audio):
     """
     Convert PCM 32-bit float audio to 16-bit audio.
     """
-    assert (audio.dtype == np.float32)
+    assert audio.dtype == np.float32
     bits = np.iinfo(np.int16).bits
-    audio = audio * (2**(bits - 1))
+    audio = audio * (2 ** (bits - 1))
     audio = np.round(audio).astype("int16")
     return audio
